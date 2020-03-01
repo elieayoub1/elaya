f1b72b09bd7251310c7428f7cf42efe9814decc9
elieayoub1 <45274696+elieayoub1@users.noreply.github.com>
2020-03-01 23:16:23 +0200

    Update menu.js

--- docs/menu.js
+++ docs/menu.js
@@ -1,4 +1,4 @@
 document.write("<li><a href='index.html'><span>Home<\/span><\/a><\/li>");
 document.write("<li><a href='page2.html'><span>End Of Day (EOD)<\/span><\/a><\/li>");
-document.write("<li><a href='page3.html'><span>Database Buckup<\/span><\/a><\/li>");
+document.write("<li><a href='page3.html'><span>Database Backup<\/span><\/a><\/li>");
 document.write("<li><a href='page3.html'><span>Oracle<\/span><\/a><\/li>");
