017d3a563106e9c9be901c809668104ed4c83d24
elieayoub1 <45274696+elieayoub1@users.noreply.github.com>
2020-03-01 15:25:31 +0300

    Create menu.js

---
+++ docs/menu.js
@@ -0,0 +1,4 @@
+document.write("<li><a href='index.html'><span>EOD<\/span><\/a><\/li>");
+document.write("<li><a href='page2.html'><span>Bulk Upload<\/span><\/a><\/li>");
+document.write("<li><a href='page3.html'><span>Database Buckup<\/span><\/a><\/li>");
+document.write("<li><a href='page4.html'><span>Oracle<\/span><\/a><\/li>");
