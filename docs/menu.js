d8f792bfc6c56dcc16f50d6e59efae09368c8171
elieayoub1 <45274696+elieayoub1@users.noreply.github.com>
2020-03-02 16:14:54 +0300

    Delete menu.js

--- docs/menu.js
+++
@@ -1,3 +0,0 @@
-document.write("<a href='#'><span>Oracle<\/span><\/a>");
-document.write("<li><a href='eod.html'><span>End Of Day (EOD)<\/span><\/a><\/li>");
-document.write("<li><a href='dbbackup.html'><span>Database Backup<\/span><\/a><\/li>");
