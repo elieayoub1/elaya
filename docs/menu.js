2088245c6bd14dcbdbc77f61aa53df99eb6cd586
elieayoub1 <45274696+elieayoub1@users.noreply.github.com>
2020-03-16 21:28:16 +0200

    Add files via upload

--- docs/menu.js
+++ docs/menu.js
@@ -2,4 +2,4 @@ document.write("<a href='#'><span>Oracle<\/span><\/a>");
 document.write("<li><a href='eod.html'><span>End Of Day (EOD)<\/span><\/a><\/li>");
 document.write("<li><a href='dbbackup.html'><span>Database Backup<\/span><\/a><\/li>");
 document.write("<li><a href='bulkupload1.html'><span>Bulk Upload - Using Template<\/span><\/a><\/li>");
-document.write("<li><a href='bulkupload2.html'><span>Bulk Upload - Using Upload Tables<\/span><\/a><\/li>");
+document.write("<li><a href='creports.html'><span>Canned Reports<\/span><\/a><\/li>");
